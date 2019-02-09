2f014599accb7efcffa216a47bdfa023077f1eef
Robert Dezso <robi@dpb.hu>
2019-02-09 18:35:32 +0100

    Added options parameter for speakingurl

--- src/Slug.php
+++ src/Slug.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Benjaminhirsch\NovaSlugField;
 
+use Laravel\Nova\Element;
 use Laravel\Nova\Fields\Field;
 
 class Slug extends Field
@@ -13,4 +14,16 @@ class Slug extends Field
      * @var string
      */
     public $component = 'nova-slug-field';
+
+    /**
+     * Specify options to pass to speakingurl.
+     *
+     * @param array $options
+     *
+     * @return $this
+     */
+    public function slugifyOptions(array $options): Element
+    {
+        return $this->withMeta(['slugifyOptions' => $options]);
+    }
 }
