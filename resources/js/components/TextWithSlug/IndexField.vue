c4e2b2cd661347d2e9369d9f5102b7a5f5a874e0
Noël Hagestein <6616996+Neol3108@users.noreply.github.com>
2021-06-28 08:18:49 +0200

    Make TextWithSlug index field inline with regular text field

--- resources/js/components/TextWithSlug/IndexField.vue
+++ resources/js/components/TextWithSlug/IndexField.vue
@@ -1,9 +1,24 @@
 <template>
-    <span>{{ field.value }}</span>
+    <div :class="`text-${field.textAlign}`">
+        <template v-if="hasValue">
+            <div v-if="field.asHtml" v-html="field.value"></div>
+            <span v-else class="whitespace-no-wrap">{{ field.value }}</span>
+        </template>
+        <p v-else>&mdash;</p>
+  </div>
 </template>
 
 <script>
 export default {
     props: ['resourceName', 'field'],
+
+    computed: {
+        /**
+         * Determine if the field has a value other than null.
+         */
+        hasValue() {
+            return this.field.value !== null
+        },
+    },
 }
 </script>
