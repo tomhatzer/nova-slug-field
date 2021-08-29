ed1e89dda16f1a98f262dfcdfbad207dda55444d
Niek Brekelmans <niek.brekelmans@gmail.com>
2021-08-29 18:06:17 +0200

    show help text

--- resources/js/components/TextWithSlug/FormField.vue
+++ resources/js/components/TextWithSlug/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field" :errors="errors">
+    <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
         <template slot="field">
             <input
                 class="w-full form-control form-input form-input-bordered"
